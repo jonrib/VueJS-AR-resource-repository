8193a508e4523568483a31a6e71bf71bce51a0e4
jonrib <j.ribikauskas@sekasoft.com>
2020-03-15 22:33:14 +0200

    Added file uploading, downloading, deleting

--- src/components/resourceEntry.vue
+++ src/components/resourceEntry.vue
@@ -60,6 +60,70 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+	<v-dialog
+      v-model="dialogDelPreviewImage"
+      max-width="450"
+    >
+      <v-card>
+        <v-card-title class="headline">Permanently delete selected preview image?</v-card-title>
+
+        <v-card-text>
+          WARNING: image will be permanently deleted!
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="darken-1"
+            text
+            @click="dialogDelPreviewImage = false"
+          >
+            Disagree
+          </v-btn>
+
+          <v-btn
+            color="red darken-1"
+            text
+            @click="deletePreviewImage(); dialogDelPreviewImage = false"
+          >
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+	<v-dialog
+      v-model="dialogDelFile"
+      max-width="450"
+    >
+      <v-card>
+        <v-card-title class="headline">Permanently delete selected file?</v-card-title>
+
+        <v-card-text>
+          WARNING: file will be permanently deleted!
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="darken-1"
+            text
+            @click="dialogDelFile = false"
+          >
+            Disagree
+          </v-btn>
+
+          <v-btn
+            color="red darken-1"
+            text
+            @click="deleteFile(); dialogDelFile = false"
+          >
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 	  <v-alert type="success" v-if="showSuccess" dismissible transition="fade-transition">
 		  {{ successText }}
 	  </v-alert>
@@ -134,6 +198,13 @@
 		multiple
 		accept="image/*"
 	></v-file-input>
+	<v-file-input
+		label="Upload 3D models and other files"
+		prepend-icon="mdi-paperclip"
+		v-if="isEditing && id!='new'"
+		v-model="uploadedFiles"
+		multiple
+	></v-file-input>
 
       <v-btn
         :disabled="!valid"
@@ -144,6 +215,24 @@
       >
         {{id == "new" ? "Create" : "Update"}}
       </v-btn>
+	  <v-data-table
+			:headers="uploadedFilesHeaders"
+			:items="uploadedPrevFiles"
+			:items-per-page="5"
+			class="elevation-1"
+		>
+			<template v-slot:item="row">
+				<tr>
+					<td><a :href="path+'/resourceEntries/'+id+'/files/'+row.item.id" target="_blank">{{row.item.name}}</a></td>
+					<td>{{row.item.size}}</td>
+					<td v-if="isEditing">
+						
+							<v-icon @click="dialogDelFile = true; fileToDel = row.item.id">mdi-delete</v-icon>
+						
+					</td>
+				</tr>
+			</template>
+			</v-data-table>
 	  </v-container>
 	  <v-container fluid class="float-right col-6">
 	  <v-carousel
@@ -168,7 +257,19 @@
 			:items-per-page="5"
 			class="elevation-1"
 			v-if="isEditing"
-		></v-data-table>
+		>
+			<template v-slot:item="row">
+				<tr>
+					<td>{{row.item.name}}</td>
+					<td>{{row.item.size}}</td>
+					<td>
+						
+							<v-icon @click="dialogDelPreviewImage = true; imagePrevToDel = row.item.id">mdi-delete</v-icon>
+						
+					</td>
+				</tr>
+			</template>
+			</v-data-table>
 		  </v-container>
 		  
 	  </v-form>	
@@ -196,8 +297,23 @@
 	  editors: [],
 	  path: '',
 	  dialog: false,
+	  dialogDelPreviewImage: false,
+	  dialogDelFile: false,
+	  imagePrevToDel: '',
+	  fileToDel: '',
 	  previewImageFiles: [],
-      previewImages: [],
+	  previewImages: [],
+	  uploadedPrevFiles: [],
+	  uploadedFilesHeaders: [
+          {
+            text: 'File',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Size in bytes', value: 'size' },
+	  ],
+	  uploadedFiles: [],
 	  isEditing: window.location.href.substring(window.location.href.lastIndexOf('/')+1,window.location.href.length) == 'new',
 	  cats: ['Architecture', 'Science', 'People', 'Misc'],
 	  id: window.location.href.substring(window.location.href.lastIndexOf('/')+1,window.location.href.length),
@@ -226,7 +342,10 @@
 			this.axios.get(this.globalBackEndPath+"/resourceEntries/"+this.id).then((data)=>{
 				this.title = data.data.title; this.description = data.data.description; this.isPrivate = data.data.private; this.tags = data.data.tags.join(';'); this.category = data.data.category; this.author = data.data.author[0].username; for(var i = 0; i < data.data.readers.length; i++){this.readers.push(data.data.readers[i].username)} for(var i = 0; i < data.data.editors.length; i++){this.editors.push(data.data.editors[i].username)} for(var i=0;i<data.data.images.length;i++){this.previewImages.push(data.data.images[i])}
 				for (var i = 0;i < data.data.images.length; i++){
-					this.uploadedPreviewImageFiles.push({name: data.data.images[i].fileName, size: data.data.images[i].size})
+					this.uploadedPreviewImageFiles.push({name: data.data.images[i].fileName, size: data.data.images[i].size, id: data.data.images[i].id})
+				}
+				for (var i = 0;i < data.data.files.length; i++){
+					this.uploadedPrevFiles.push({name: data.data.files[i].fileName, size: data.data.files[i].size, id: data.data.files[i].id})
 				}
 			}).catch((error) => {this.failText = error.response.data; this.showFail = true; setTimeout(()=>{this.showFail=false},3000)});
 		}else{
@@ -244,9 +363,9 @@
 				this.globalBackEndPath+"/resourceEntries/"+this.id,
 				payload
 			  ).then(() => {
-				  this.successText = "Successfuly updated entry!" + (this.previewImageFiles.length == 0 ? '' : ' Will upload images!');
+				  this.successText = "Successfuly updated entry!" + (this.previewImageFiles.length == 0 ? '' : ' Will upload images!') + (this.previewImageFiles.length == 0 ? '' : ' Will upload images!');
 				  this.showSuccess = true;
-				  if (this.previewImageFiles.length == 0){
+				  if (this.previewImageFiles.length == 0 && this.uploadedFiles.length == 0){
 					  setTimeout(()=>{this.showSuccess=false;window.location.href="/resources"},3000);
 				  }else{
 					setTimeout(()=>{this.showSuccess=false;},3000);
@@ -259,16 +378,36 @@
 							formData.append('file', file);
 					}
 					var imageUrl =this.globalBackEndPath+"/resourceEntries/"+this.id+"/previewImages";
-					this.axios.post(imageUrl, 
-					formData,
-					{
-						headers: {
-							'Content-Type': 'multipart/form-data'
-						}
-					}).then(() => {this.successText = "Updated preview images!";this.showSuccess = true;setTimeout(()=>{this.showSuccess=false;},3000);window.location.href="/resources"}).catch((error)=>{this.failText = error.response.data; this.showFail = true; setTimeout(()=>{this.showFail=false},3000)})
+					if (this.previewImageFiles.length != 0){
+						this.axios.post(imageUrl, 
+						formData,
+						{
+							headers: {
+								'Content-Type': 'multipart/form-data'
+							}
+						}).then(() => {this.successText = "Updated preview images!";this.showSuccess = true;setTimeout(()=>{this.showSuccess=false;},3000);if (this.uploadedFiles.length == 0){window.location.href="/resources"}}).catch((error)=>{this.failText = error.response.data; this.showFail = true; setTimeout(()=>{this.showFail=false},3000)})
+					}
+					let formData2 = new FormData();
+					for( var i = 0; i < this.uploadedFiles.length; i++ ){
+						let file = this.uploadedFiles[i];
+						//TODO
+						//validate size
+						formData2.append('file', file);
+					}
+					var fileUrl =this.globalBackEndPath+"/resourceEntries/"+this.id+"/files";
+					if (this.uploadedFiles.length != 0){
+						this.axios.post(fileUrl, 
+						formData2,
+						{
+							headers: {
+								'Content-Type': 'multipart/form-data'
+							}
+						
+						}).then(() => {this.successText = "Updated uploaded files!";this.showSuccess = true;setTimeout(()=>{this.showSuccess=false;},3000);window.location.href="/resources"}).catch((error)=>{this.failText = error.response.data; this.showFail = true; setTimeout(()=>{this.showFail=false},3000)})
+					}
 				  }
+				}).catch((error) => {this.failText = error.response.data; this.showFail = true; setTimeout(()=>{this.showFail=false},3000)});
 				  
-		        }).catch((error) => {this.failText = error.response.data; this.showFail = true; setTimeout(()=>{this.showFail=false},3000)});
 		  }else{
 			this.axios.post(
 				this.globalBackEndPath+"/resourceEntries",
@@ -290,7 +429,46 @@
 	  },
 	  deleteEntry (){
 		  this.axios.delete(this.globalBackEndPath+"/resourceEntries/"+this.id).then(()=>{this.successText = "Success!"; this.showSuccess = true; setTimeout(()=>{this.showSuccess=false;window.location.href="/resources"},3000)}).catch((error)=>{this.failText = error.response.data; this.showFail = true; setTimeout(()=>{this.showFail=false},3000)});
-	  }
+	  },
+	  deletePreviewImage (){
+		  this.axios.delete(this.globalBackEndPath+"/resourceEntries/"+this.id+"/previewImages/"+this.imagePrevToDel).then(()=>{
+			  this.successText = "Success!";
+			  this.showSuccess = true;
+			  setTimeout(()=>{this.showSuccess=false;},3000);
+			  var ind1 = -1;
+			  var ind2 = -1;
+			  for(var i = 0; i < this.previewImages.length; i++){
+				  if (this.previewImages[i].id == this.imagePrevToDel){
+					ind1 = i;
+					break;
+				  }
+			  } 
+			  this.previewImages.splice(ind1, 1);
+			  for(var i = 0; i < this.uploadedPreviewImageFiles.length; i++){
+				  if (this.uploadedPreviewImageFiles[i].id == this.imagePrevToDel){
+					ind2 = i;
+					break;
+				  }
+				  
+			  } 
+			  this.uploadedPreviewImageFiles.splice(ind2, 1);
+		   }).catch((error)=>{this.failText = error.response.data; this.showFail = true; setTimeout(()=>{this.showFail=false},3000)});
+	  },
+	  deleteFile (){
+		  this.axios.delete(this.globalBackEndPath+"/resourceEntries/"+this.id+"/files/"+this.fileToDel).then(()=>{
+			  this.successText = "Success!";
+			  this.showSuccess = true;
+			  setTimeout(()=>{this.showSuccess=false;},3000);
+			  var ind1 = -1;
+			  for(var i = 0; i < this.uploadedPrevFiles.length; i++){
+				  if (this.uploadedPrevFiles[i].id == this.fileToDel){
+					ind1 = i;
+					break;
+				  }
+			  } 
+			  this.uploadedPrevFiles.splice(ind1, 1);
+		   }).catch((error)=>{this.failText = error.response.data; this.showFail = true; setTimeout(()=>{this.showFail=false},3000)});
+	  },
     },
   }
 </script>
